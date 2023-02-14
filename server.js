e4cba42ea6644950bc5244a4147435c26481c2e5
Jatin pandey <jatin26pandey@gmail.com>
2023-02-14 22:11:44 +0530

    cors

--- server.js
+++ server.js
@@ -1,30 +1,31 @@
-import express from 'express'
-import {connect} from './database.js'
-import dotenv from 'dotenv' 
-import router from './Routes/auth.js';
-import cookieParser from 'cookie-parser';
+import express from "express";
+import { connect } from "./database.js";
+import dotenv from "dotenv";
+import router from "./Routes/auth.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
 const app = express();
- 
+
 dotenv.config({
-    path: './.env'
-})
+  path: "./.env",
+});
 
 connect();
 
 app.use(express.json());
 
+app.use(cors());
+
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/v1/',router)
+app.use("/api/v1/", router);
 
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-
-
 app.listen(4000, () => {
-  console.log('Example app listening on port 4000!');
-}); 
+  console.log("Example app listening on port 4000!");
+});
