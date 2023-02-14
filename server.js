b667831355dc443a3a788fe6dc273b5977da2aa1
Jatin pandey <jatin26pandey@gmail.com>
2023-02-14 22:17:40 +0530

    cors

--- server.js
+++ server.js
@@ -15,7 +15,13 @@ connect();
 
 app.use(express.json());
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
